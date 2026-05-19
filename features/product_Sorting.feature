f042b351da13877d251d94466bf5b25ddd1af191
Muskan <muskanpatel3102@gmail.com>
2026-05-19 11:33:10 +0530

    product Sorting added

--- features/product_Sorting.feature
+++ features/product_Sorting.feature
@@ -1,1 +1,7 @@
 #Muskan
+Feature: Product Sorting
+
+  Scenario: Sort products by price
+    Given user is on category page
+    When user sorts products by price low to high
+    Then products should be sorted successfully
--- step_definitions/test_product_Sorting_steps.py
+++ step_definitions/test_product_Sorting_steps.py
@@ -1,2 +1,29 @@
-#muskan
-#comment
+from pytest_bdd import scenarios, given, when, then
+from pages.product_Sorting import ProductSortingPage
+
+
+scenarios("../features/product_Sorting.feature")
+
+
+@given("user is on category page")
+def open_category(browser):
+
+    sorting = ProductSortingPage(browser)
+
+    sorting.open_category_page()
+
+
+@when("user sorts products by price low to high")
+def sort_products(browser):
+
+    sorting = ProductSortingPage(browser)
+
+    sorting.sort_products()
+
+
+@then("products should be sorted successfully")
+def verify_sorting(browser):
+
+    sorting = ProductSortingPage(browser)
+
+    assert sorting.verify_sorting()
