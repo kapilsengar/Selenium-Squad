623cbdb9f673cc443610a8222912f281d23553c1
Harshu9144 <harshuverma9144@gmail.com>
2026-05-19 11:15:15 +0530

    search feature added

--- step_definitions/test_search_steps.py
+++ step_definitions/test_search_steps.py
@@ -1,1 +1,27 @@
-#done
+from pytest_bdd import scenarios, given, when, then
+from pages.search_page import SearchPage
+
+
+scenarios("../features/search.feature")
+
+
+@given("user is on homepage")
+def open_homepage(browser):
+    search = SearchPage(browser)
+    search.open_homepage()
+
+
+@when("user searches for product")
+def search_product(browser):
+    search = SearchPage(browser)
+
+    search.search_product(
+        "Build your own cheap computer"
+    )
+
+
+@then("searched product should be displayed")
+def verify_product(browser):
+    search = SearchPage(browser)
+
+    assert search.is_product_displayed()
