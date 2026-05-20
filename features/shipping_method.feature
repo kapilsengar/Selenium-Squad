028029af2ab04283c22e52c6a4fcf74b8d1fc300
Ashutosh Thakur <aashutosh021128@gmail.com>
2026-05-20 11:33:12 +0530

    Adding Shipping method

--- features/shipping_method.feature
+++ features/shipping_method.feature
@@ -1,1 +1,6 @@
-#Ashutosh
+Feature: Shipping Method
+
+  Scenario: Select shipping method
+    Given user is on shipping method page
+    When user selects shipping method
+    Then shipping method should be selected
--- step_definitions/test_shipping_method.py
+++ step_definitions/test_shipping_method.py
@@ -1,1 +1,34 @@
-# Ashutosh - Test Shipping Method
+from pytest_bdd import scenarios, given, when, then
+from pages.shipping_method import ShippingMethodPage
+
+
+scenarios("../features/shipping_method.feature")
+
+
+@given("user is on shipping method page")
+def shipping_page(browser):
+
+    shipping = ShippingMethodPage(browser)
+
+    shipping.open_login_page()
+    shipping.login()
+    shipping.add_product()
+    shipping.proceed_checkout()
+
+    shipping.fill_billing_address()
+    shipping.continue_billing()
+
+
+@when("user selects shipping method")
+def select_shipping(browser):
+
+    shipping = ShippingMethodPage(browser)
+    shipping.complete_checkout_steps()
+
+   
+
+
+@then("shipping method should be selected")
+def verify_shipping(browser):
+
+    assert True
