116deb09156e1c688a22817ff724beb4cdf06f11
Kapil <kapilsengar49@gmail.com>
2026-05-21 16:20:13 +0530

    Updating Base Page And one utils folder and test data file for fetch data

--- pages/invalid_login_page.py
+++ pages/invalid_login_page.py
@@ -1,61 +1,40 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
+from utils.test_data import get_invalid_login_data
 
 
-class InvalidLoginPage:
+class InvalidLoginPage(BasePage):
+    # Login page endpoint
+    LOGIN_ENDPOINT = "/login"
 
+    # Web elements
     EMAIL = (By.ID, "Email")
-
     PASSWORD = (By.ID, "Password")
-
-    LOGIN_BUTTON = (
-        By.CSS_SELECTOR,
-        "input.login-button"
-    )
-
-    ERROR_MESSAGE = (
-        By.CSS_SELECTOR,
-        "div.validation-summary-errors"
-    )
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "input.login-button")
+    ERROR_MESSAGE = (By.CSS_SELECTOR, "div.validation-summary-errors")
 
     def __init__(self, driver):
+        super().__init__(driver)
 
-        self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
-
+    # Open login page
     def open_login_page(self):
+        self.open_url(self.LOGIN_ENDPOINT)
 
-        self.driver.get(
-            "https://demowebshop.tricentis.com/login"
-        )
-
+    # Enter invalid credentials
     def enter_invalid_credentials(self):
+        email, password = get_invalid_login_data()
 
-        self.wait.until(
-            EC.visibility_of_element_located(
-                self.EMAIL
-            )
-        ).send_keys("wrong@gmail.com")
+        self.wait.until(EC.visibility_of_element_located(self.EMAIL)).send_keys(email)
 
-        self.driver.find_element(
-            *self.PASSWORD
-        ).send_keys("WrongPassword")
+        self.driver.find_element(*self.PASSWORD).send_keys(password)
 
+    # Click login button
     def click_login(self):
+        self.wait.until(EC.element_to_be_clickable(self.LOGIN_BUTTON)).click()
 
-        self.wait.until(
-            EC.element_to_be_clickable(
-                self.LOGIN_BUTTON
-            )
-        ).click()
-
+    # Verify error message
     def verify_error_message(self):
-
-        error = self.wait.until(
-            EC.visibility_of_element_located(
-                self.ERROR_MESSAGE
-            )
-        )
+        error = self.wait.until(EC.visibility_of_element_located(self.ERROR_MESSAGE))
 
         return "Login was unsuccessful" in error.text
--- pages/remove_cart_page.py
+++ pages/remove_cart_page.py
@@ -1,121 +1,68 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
 
 
-class RemoveCartPage:
+class RemoveCartPage(BasePage):
+    # Page endpoints
+    HOME_ENDPOINT = "/"
+    PRODUCT_ENDPOINT = "/build-your-cheap-own-computer"
+    CART_ENDPOINT = "/cart"
 
-    PRODUCT = (
-        By.LINK_TEXT,
-        "Build your own cheap computer"
-    )
+    # Web elements
+    ADD_TO_CART_BUTTON = (By.XPATH, "//input[contains(@class,'add-to-cart-button')]")
 
-    ADD_TO_CART_BUTTON = (
-        By.CSS_SELECTOR,
-        "input[value='Add to cart']"
-    )
+    SHOPPING_CART = (By.LINK_TEXT, "Shopping cart")
 
-    SHOPPING_CART = (
-        By.LINK_TEXT,
-        "Shopping cart"
-    )
+    REMOVE_CHECKBOX = (By.XPATH, "//input[contains(@name,'removefromcart')]")
 
-    REMOVE_CHECKBOX = (
-        By.XPATH,
-        "//input[contains(@name,'removefromcart')]"
-    )
+    UPDATE_CART_BUTTON = (By.NAME, "updatecart")
 
-    
-    UPDATE_CART_BUTTON = (
-        By.NAME,
-        "updatecart"
-    )
-
-    EMPTY_CART_MESSAGE = (
-        By.CSS_SELECTOR,
-        "div.order-summary-content"
-    )
+    EMPTY_CART_MESSAGE = (By.CSS_SELECTOR, "div.order-summary-content")
 
     def __init__(self, driver):
-        self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        super().__init__(driver)
 
+    # Open homepage
     def open_homepage(self):
-        self.driver.get(
-            "https://demowebshop.tricentis.com/"
-        )
+        self.open_url(self.HOME_ENDPOINT)
 
+    # Add product to cart
     def add_product_to_cart(self):
+        self.open_url(self.PRODUCT_ENDPOINT)
 
-        self.driver.get(
-            "https://demowebshop.tricentis.com/build-your-cheap-own-computer"
-        )
+        add_cart = self.wait.until(EC.element_to_be_clickable(self.ADD_TO_CART_BUTTON))
 
-        add_cart = self.wait.until(
-            EC.presence_of_element_located(
-                (
-                    By.XPATH,
-                    "//input[contains(@class,'add-to-cart-button')]"
-                )
-            )
-        )
+        self.driver.execute_script("arguments[0].click();", add_cart)
 
-        self.driver.execute_script(
-            "arguments[0].click();",
-            add_cart
-        )
+        self.open_url(self.CART_ENDPOINT)
 
-        self.driver.get(
-            "https://demowebshop.tricentis.com/cart"
-        )
-
+    # Open shopping cart
     def open_cart(self):
+        self.wait.until(EC.element_to_be_clickable(self.SHOPPING_CART)).click()
 
-        self.wait.until(
-            EC.element_to_be_clickable(
-                self.SHOPPING_CART
-            )
-        ).click()
-
+    # Remove product from cart
     def remove_product(self):
-
         try:
-
             remove_checkbox = self.wait.until(
-                EC.presence_of_element_located(
-                    (
-                        By.XPATH,
-                        "//input[contains(@name,'removefromcart')]"
-                    )
-                )
+                EC.element_to_be_clickable(self.REMOVE_CHECKBOX)
             )
 
-            self.driver.execute_script(
-                "arguments[0].click();",
-                remove_checkbox
-            )
+            self.driver.execute_script("arguments[0].click();", remove_checkbox)
 
             update_button = self.wait.until(
-                EC.presence_of_element_located(
-                    self.UPDATE_CART_BUTTON
-                )
+                EC.element_to_be_clickable(self.UPDATE_CART_BUTTON)
             )
 
-            self.driver.execute_script(
-                "arguments[0].click();",
-                update_button
-            )
+            self.driver.execute_script("arguments[0].click();", update_button)
 
         except:
             print("Cart already empty")
 
-            
+    # Verify empty cart
     def verify_cart_empty(self):
-
         message = self.wait.until(
-            EC.visibility_of_element_located(
-                self.EMPTY_CART_MESSAGE
-            )
+            EC.visibility_of_element_located(self.EMPTY_CART_MESSAGE)
         )
 
         return "Your Shopping Cart is empty!" in message.text
